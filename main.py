cfd839d745e1568cb995cadfa62d0f4b356eb6d3
Monolocker <Gem.trigg69@yahoo.com>
2026-01-08 15:04:31 -0600

    imported text_report

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from stats import word_count
 from stats import char_count
+from stats import text_report
 
-book_path = "books/frankenstein.txt" # use relative path books/frankenstein.txt, in global scope
+book_path = "books/frankenstein.txt" # use relative path books/frankenstein.txt
 
-# book_path can also be in scope of main function 
+# book_path can also be in local scope of main function 
 def main(): 
     text = get_book_text(book_path)
     num_words = word_count(text)
+    char_dict = char_count(text)
     print(f"Found {num_words} total words")
     print(char_count(text))
+    print(text_report(char_dict))
 
 def get_book_text(book_path):
     with open(book_path) as f:
