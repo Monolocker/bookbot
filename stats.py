6a3ce39d1084bc2c58501265e8790911907348b7
Monolocker <Gem.trigg69@yahoo.com>
2026-01-08 15:02:52 -0600

    text_report created

--- stats.py
+++ stats.py
@@ -4,19 +4,28 @@ def word_count(text):
 
 def char_count(text):
     lower_text = text.lower()
-    character_dict = {}
+    char_dict = {}
+
     for characters in lower_text:
          # iterate through each char in lower_text
-        if characters in character_dict: 
+        if characters in char_dict: 
             # check if current char is a key already in dict
             # if so, char encountered before
-            character_dict[characters] += 1
+            char_dict[characters] += 1
             # thus char already in dict, increment count by 1
         else: 
             # if char not yet in dict, else block executed
             # first time char encountered
-            character_dict[characters] = 1
+            char_dict[characters] = 1
             # add new char to dict as key and set initial count to 1
-    return character_dict
+    return char_dict
 
+def text_report(char_dict):
+    dict_list = []
     
+    for key, value in char_dict.items():
+        char_info = {"char": key, "num": value}
+        dict_list.append(char_info)
+
+    return dict_list
+
