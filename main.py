bf0e0c98448c388e457a778454d0c04584397a87
Monolocker <Gem.trigg69@yahoo.com>
2026-01-09 14:41:47 -0600

    print character frequency report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import word_count
 from stats import char_count
-from stats import text_report
+from stats import char_dict_to_sorted_list
 
 book_path = "books/frankenstein.txt" # use relative path books/frankenstein.txt
 
@@ -9,14 +9,19 @@ def main():
     text = get_book_text(book_path)
     num_words = word_count(text)
     char_dict = char_count(text)
+    char_sorted_list = char_dict_to_sorted_list(char_dict)
     print(f"Found {num_words} total words")
-    print(char_count(text))
-    print(text_report(char_dict))
+    for item in char_sorted_list: 
+        character = item["char"] # extract char
+        count = item["num"]      # extract count
+        if character.isalpha():  # only include letters
+            print(f"{character}: {count}")
 
 def get_book_text(book_path):
     with open(book_path) as f:
         read_frankenstein = f.read() # can make this simply return f.read()
     
     return read_frankenstein
+
 
 main()
