0ce70f614d317a0ea296eff0323110ee76f7eb34
Monolocker <Gem.trigg69@yahoo.com>
2026-01-13 14:45:59 -0600

    added command-line argument support for book path

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from stats import word_count
 from stats import char_count
 from stats import char_dict_to_sorted_list
+import sys
 
-book_path = "books/frankenstein.txt" # use relative path books/frankenstein.txt
+# check if exactly 2 args provided (script name + book path)
+if len(sys.argv) != 2: 
+        # show usage instructions if wrong number of args
+        print("Usage: python3 main.py <path_to_book>")
+        # exit program with error code 1 indicating failure
+        sys.exit(1)
 
-# book_path can also be in local scope of main function 
+# get book path from command line argument
+book_path = sys.argv[1]
+
 def main(): 
     text = get_book_text(book_path)
     num_words = word_count(text)
@@ -22,6 +30,5 @@ def get_book_text(book_path):
         read_frankenstein = f.read() # can make this simply return f.read()
     
     return read_frankenstein
-
 
 main()
